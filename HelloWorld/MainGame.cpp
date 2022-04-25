48806d66e91544d352baf96049087c55d89c1f0d
JayK1tsune <jdforrest22@gmail.com>
2022-04-25 14:22:01 +0100

    Update MainGame.cpp

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 #define PLAY_IMPLEMENTATION
 #define PLAY_USING_GAMEOBJECT_MANAGER
 #include "Play.h"
+=======
+
+#include "Player.h"
+>>>>>>> parent of 60b24cf (Merge branch 'HelloWorld' of https://github.com/JayK1tsune/playbuffer into HelloWorld)
 
 int DISPLAY_WIDTH = 1280;
 int DISPLAY_HEIGHT = 720;
 int DISPLAY_SCALE = 1;
 
+<<<<<<< HEAD
 struct Gamestate
 {
 	float timer = 0;
@@ -19,11 +25,31 @@ void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
 	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
 	Play::CentreAllSpriteOrigins();
 	//Play::LoadBackground("E:\\UNI\\playbuffer\\HelloWorld\\Data\\Backgrounds\\Frogger.png");
+=======
+Player player = Player();
+
+struct GameState
+{
+	float timer = 0;
+	int spriteID = 0;
+};
+
+GameState gameState;
+
+// The entry point for a PlayBuffer program
+//:: = scope 
+void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
+{
+	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+
+	player.InitPlayer("paddle", DISPLAY_WIDTH * 0.5f, DISPLAY_HEIGHT * 0.75);
+>>>>>>> parent of 60b24cf (Merge branch 'HelloWorld' of https://github.com/JayK1tsune/playbuffer into HelloWorld)
 }
 
 // Called by PlayBuffer every frame (60 times a second!)
 bool MainGameUpdate( float elapsedTime )
 {
+<<<<<<< HEAD
 	Play::ClearDrawingBuffer( Play::cOrange );
 	Play::DrawDebugText({ DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 },
 		Play::GetSpriteName(gameState.spriteId),
@@ -34,6 +60,24 @@ bool MainGameUpdate( float elapsedTime )
 	//Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Hello World!" );
 	Play::PresentDrawingBuffer();
 	return Play::KeyDown( VK_ESCAPE );
+=======
+<<<<<<< HEAD
+	Play::ClearDrawingBuffer( Play::cOrange );
+	Play::DrawDebugText( { DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 }, "Frogger!" );
+=======
+	Play::ClearDrawingBuffer( Play::cBlack);
+
+	//Play::DrawDebugText({ DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2 },
+	//	Play::GetSpriteName(gameState.spriteID),
+	//	Play::cWhite);
+
+	player.UpdatePlayer();
+
+>>>>>>> fedd651f216a26e0fd1718cfcbb9e4ec12a6311d
+	Play::PresentDrawingBuffer();
+	return Play::KeyDown( VK_ESCAPE );
+	
+>>>>>>> parent of 60b24cf (Merge branch 'HelloWorld' of https://github.com/JayK1tsune/playbuffer into HelloWorld)
 }
 
 // Gets called once when the player quits the game 
